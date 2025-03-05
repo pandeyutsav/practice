5a93a461cfca6e8007e50d94ca0db2ce5e604229
pandeyutsav <103882589+pandeyutsav@users.noreply.github.com>
2025-03-05 12:40:20 +0530

    4th

---
+++ Day1/4th.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+    
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr.begin(),arr.end());
+
+    int i=n/2,j=n-1;
+    while(i<j)
+    {
+        swap(arr[i],arr[j]);
+        i++,j--;
+    }
+    for(int i = 0; i < n; i++)
+        cout << arr[i]<<" ";
+
+}
